476804f9e90e048b22091826f70e3c528062a718
maskleo <masque@aliyun.com>
2018-06-09 16:47:43 +0800

    add button

--- app/src/main/java/maskleo/edu/androiddemo1/MainActivity.java
+++ app/src/main/java/maskleo/edu/androiddemo1/MainActivity.java
@@ -2,6 +2,7 @@ package maskleo.edu.androiddemo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView v = this.findViewById(R.id.helloText);
+        v.setText("This is first textview!");
     }
 }
