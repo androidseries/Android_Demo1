64c61c7012e9e58efb25e150ab8d10da721c2f7e
maskleo <masque@aliyun.com>
2018-06-09 13:36:46 +0800

    Initial commit

---
+++ app/src/main/java/maskleo/edu/androiddemo1/MainActivity.java
@@ -0,0 +1,13 @@
+package maskleo.edu.androiddemo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
