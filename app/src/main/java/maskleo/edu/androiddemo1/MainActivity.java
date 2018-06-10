15f1fcdb505b9589fd4b8bfbc833aa8b539fd00e
maskleo <masque@aliyun.com>
2018-06-10 21:03:23 +0800

    添加官方教程链接

--- app/src/main/java/maskleo/edu/androiddemo1/MainActivity.java
+++ app/src/main/java/maskleo/edu/androiddemo1/MainActivity.java
@@ -2,7 +2,8 @@ package maskleo.edu.androiddemo1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = this.findViewById(R.id.send);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("===========================");
+            }
+        });
     }
 }
